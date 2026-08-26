7e738589fcaa2878e12256b01ceffa070590095d
Muhammed Ahad <maaahad@gmail.com>
2026-08-26 15:14:44 +0200

    validate method for AppConfig

--- crates/app-config/src/lib.rs
+++ crates/app-config/src/lib.rs
@@ -1,3 +1,4 @@
+use config::ConfigError;
 use serde::Deserialize;
 
 #[derive(Debug, Clone, Deserialize)]
@@ -12,11 +13,20 @@ pub struct ServerConfig {
 }
 
 impl AppConfig {
-    pub fn load() -> Result<Self, config::ConfigError> {
+    pub fn load() -> Result<Self, ConfigError> {
         let builder =
             config::Config::builder().add_source(config::File::with_name("config/default"));
         // TODO:  add_source for .env file (with secret for local development)
 
         builder.build()?.try_deserialize()
+    }
+
+    pub fn validate(&self) -> Result<(), ConfigError> {
+        if self.server.port == 0 {
+            // FIXME: need right error type
+            return Err(ConfigError::Message("server port can not be 0".into()));
+        }
+
+        Ok(())
     }
 }
