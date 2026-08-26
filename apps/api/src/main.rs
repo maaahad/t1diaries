0366803efb58923e91c188a7ee4e723bd64e8009
Muhammed Ahad <maaahad@gmail.com>
2026-08-26 15:15:52 +0200

    validating config in the app

--- apps/api/src/main.rs
+++ apps/api/src/main.rs
@@ -2,6 +2,7 @@
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let config = app_config::AppConfig::load()?;
     // TODO: config validation and fails instantly if validation failed
+    config.validate()?;
 
     println!("{:?}", config);
 
