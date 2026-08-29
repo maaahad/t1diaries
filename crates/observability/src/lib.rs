cd2208787a9558fc15d16a86d13ef8c6767e663d
Muhammed Ahad <maaahad@gmail.com>
2026-08-29 13:43:49 +0200

    parsing level added for api and tower_http crated

--- crates/observability/src/lib.rs
+++ crates/observability/src/lib.rs
@@ -1,9 +1,8 @@
 pub fn init_tracing() {
-    tracing_subscriber::fmt().init();
-    // tracing_subscriber::fmt()
-    //     .with_env_filter(
-    //         std::env::var("RUST_LOG").unwrap_or_else(|_| "t1diaries=info, tower_http=info".into()),
-    //     )
-    //     .json()
-    //     .init();
+    // TODO: (maaahad) subscribe other dependencies/crates's parsing level
+    tracing_subscriber::fmt()
+        .with_env_filter(
+            std::env::var("RUST_LOG").unwrap_or_else(|_| "api=info,tower_http=info".into()),
+        )
+        .init();
 }
