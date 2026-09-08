06eebb866eb81a213f6ae9093321819241f3f904
Muhammed Ahad <maaahad@gmail.com>
2026-09-08 21:43:19 +0200

    TestAppConfig updated to have DatabaseConig

--- apps/api/tests/common/config.rs
+++ apps/api/tests/common/config.rs
@@ -1,4 +1,5 @@
 use app_config::{AppConfig, Environment, GraphqlConfig, MetaConfig, ServerConfig};
+use database::config::DatabaseConfig;
 
 pub struct TestAppConfig {
     config: AppConfig,
@@ -17,6 +18,11 @@ impl TestAppConfig {
                     port: 0,
                 },
                 graphql: GraphqlConfig { graphiql: false },
+                database: DatabaseConfig {
+                    url: String::from("postgres://user:password@localhost:5432/t1diaries"),
+                    max_connections: 10,
+                    min_connections: 5,
+                },
             },
         }
     }
